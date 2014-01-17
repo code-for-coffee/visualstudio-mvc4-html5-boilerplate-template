4638c5a9515d29de0bb8aa2e36f1aba2aad40f52
code-for-coffee <jtraverjr@gmail.com>
2014-01-17 09:10:55 -0600

    Template

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace $safeprojectname$
+{
+	public class BundleConfig
+	{
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+			bundles.Add(new ScriptBundle("~/Content/themes/base/js/vendor/").Include(
+						"~/Content/themes/base/js/vendor/jquery-{version}.js"));
+
+			bundles.Add(new ScriptBundle("~/Content/themes/base/js/vendor/").Include(
+						"~/Content/themes/base/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"));
+
+			bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+						"~/Content/themes/base/css/main.css",
+						"~/Content/themes/base/css/normalize.min.css"));
+		}
+	}
+}
